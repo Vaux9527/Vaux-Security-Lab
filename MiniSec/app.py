517b47cd9997d76f11186ea1304e34867356ba59
Vaux9527 <2965306248@qq.com>
2026-08-11 10:24:11 +0800

    initially trying jumping to labs

--- MiniSec/app.py
+++ MiniSec/app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def index():
 
     return render_template("index.html")
-
+@app.route("/labs")
+def labs():
+    return "Sercurity Labs"
 
 if __name__ == "__main__":
     app.run(
