075aaf809184db07d77a0dc976356a2c5bbe1503
Vaux9527 <2965306248@qq.com>
2026-08-11 10:46:40 +0800

    creating labs.html

--- MiniSec/app.py
+++ MiniSec/app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def index():
 
     return render_template("index.html")
+
 @app.route("/labs")
 def labs():
-    return "Sercurity Labs"
+    
+    return render_template("labs.html")
 
 if __name__ == "__main__":
     app.run(
